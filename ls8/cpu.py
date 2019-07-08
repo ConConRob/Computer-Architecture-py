4fb44bfde8dd064bf4f6e2b1b25d76958094cfe8
conconrob <connortprobertson@gmail.com>
2019-07-08 01:16:42 +0100

    basic print program running

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,33 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.memory = [0] * 256
+        self.pc = 0  # program counter
+        self.r0 = 0
+        self.r1 = 0
+        self.r2 = 0
+        self.r3 = 0
+        self.r4 = 0
+        self.r5 = 0  # interrupt mask (IM)
+        self.r6 = 0  # interrupt status (IS)
+        self.r7 = 0  # stack pointer (SP)
+
+    def ram_read(self, MAR):  # memory address register
+        # return the value at the address
+        try:
+            return self.memory[MAR]
+        except IndexError:
+            return None
+
+    def ram_write(self, MDR, MAR):  # memory data register
+        # write the address to the value and don't return anthing
+        self.memory[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +39,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.memory[address] = instruction
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +82,35 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        
+        while True:
+            IR = self.pc  # instruction register
+            opcode = self.ram_read(IR)
+            operand_a = self.ram_read(IR + 1)
+            operand_b = self.ram_read(IR + 2)
+
+            if opcode == 0b10000010:  # LDI
+                # register is operand_a
+                # value is operand_b
+                self.ram_write(operand_b, operand_a)
+                setattr(self,f'r{operand_a}',operand_b)
+                # increment the pc
+                self.pc += 3
+            elif opcode == 0b01000111:  # PRN
+                # print the numeric value in register of operand_a
+                print(getattr(self, f'r{operand_a}'))
+                # increment the pc
+                self.pc += 2
+            elif opcode == 0b00000001:# HLT
+                # terminate program
+                sys.exit()
+            elif opcode == 0b1:
+                pass
+            elif opcode == 0b1:
+                pass
+            elif opcode == 0b1:
+                pass
+            elif opcode == 0b1:
+                pass
+            elif opcode == 0b1:
+                pass
